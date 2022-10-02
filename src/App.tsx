43393fdbf4e4195f536583c4487a7ba7184a6485
Gabriel Freitas <gabriel.freitas.santos@g.globo>
2022-10-02 17:51:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import { Container } from "./styles";
+
+interface Candidato {
+  nm: string;
+  pvap: string;
+  vap: string;
+}
+
+interface Candidatos {
+  cand: Candidato[];
+}
+
+function App() {
+  const [votos, setVotos] = useState<Candidato[]>([]);
+
+  useEffect(() => {
+    getInfo();
+  }, []);
+
+  const getInfo = async () => {
+    const result = await axios.get<Candidatos>(
+      "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
+    );
+
+    setVotos(result.data.cand);
+  };
+
+  return (
+    <Container>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Quantidade</th>
+          <th>Porcentagem</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {votos.map((cand, idx) => (
+          <tr key={`tr-${idx}`}>
+            <td key={`name-${idx}`}>{cand.nm}</td>
+            <td key={`qtd-${idx}`}>{cand.vap}</td>
+            <td key={`pct-${idx}`}>{cand.pvap}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.table`
+
+    border: 1px solid #000000;
+
+    th, td{
+        border: 1px solid #000000;
+    }
+    align-items: center;
+
+`
