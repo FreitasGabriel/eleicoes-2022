75cd3bedc6cccedc4d3c9bc11692f686e1c2e6d6
Gabriel Freitas <gabriel.freitas.santos@g.globo>
2022-10-30 16:21:10 -0300

    change url to second round

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,32 @@ import { Container } from "./styles";
 
 function App() {
   const [pres, setPres] = useState<Candidato[]>([]);
-  const [gov, setGov] = useState<Candidato[]>([]);
-  const [sen, setSen] = useState<Candidato[]>([]);
+  // const [gov, setGov] = useState<Candidato[]>([]);
+  // const [sen, setSen] = useState<Candidato[]>([]);
 
   useEffect(() => {
     getInfo();
   }, []);
 
   const getInfo = async () => {
-    const pres = await axios.get<Candidatos>(constants.URL_PRES);
-    const gov = await axios.get<Candidatos>(constants.URL_GOV_RJ);
-    const sen = await axios.get<Candidatos>(constants.URL_SEN_RJ);
-
+    const pres = await axios.get<Candidatos>(constants.URL_PRES, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // const gov = await axios.get<Candidatos>(constants.URL_GOV_RJ);
+    // const sen = await axios.get<Candidatos>(constants.URL_SEN_RJ);
+    console.log("pres", pres);
     setPres(pres.data.c);
-    setGov(gov.data.c);
-    setSen(sen.data.c);
+    // setGov(gov.data.c);
+    // setSen(sen.data.c);
   };
 
   return (
     <Container>
       <CandTable c={pres} title="Presidente" />
-      <CandTable c={gov} title="Governador RJ" />
-      <CandTable c={sen} title="Senador RJ" />
+      {/* <CandTable c={gov} title="Governador RJ" />
+      <CandTable c={sen} title="Senador RJ" /> */}
     </Container>
   );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const constants = {
-    URL_PRES: 'https://infograficos-eleicoes-oglobo.s3.amazonaws.com/2022/1-turno/apuracao/presidente/presidente_brasil.json',
+    URL_PRES: 'https://infograficos-eleicoes-oglobo.s3.amazonaws.com/2022/2-turno/apuracao/presidente/presidente_brasil.json',
     URL_GOV_RJ: 'https://infograficos-eleicoes-oglobo.s3.amazonaws.com/2022/1-turno/estados/governador/RJ.json',
     URL_SEN_RJ: 'https://infograficos-eleicoes-oglobo.s3.amazonaws.com/2022/1-turno/estados/senador/RJ.json'
 
